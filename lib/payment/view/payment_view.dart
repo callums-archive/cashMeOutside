b5c4c29e2a94cd7ad1e2535e128ff73d9e36a110
Callum Fleming <howzitcallum@gmail.com>
2020-08-21 18:13:41 +0200

    feat(text input only accepts currency): the text imput only takes in currency

--- lib/payment/view/payment_view.dart
+++ lib/payment/view/payment_view.dart
@@ -1,4 +1,5 @@
 import 'package:cashMeOutside/payment/bloc/payment_state.dart';
+import 'package:cashMeOutside/payment/tools/decimal_text_input_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,11 +10,17 @@ class PaymentView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("TEST")),
+        appBar: AppBar(title: Text("Payment")),
         body: Center(
           child: BlocBuilder<PaymentBloc, PaymentState>(
             builder: (context, state) {
-              return TextFormField(initialValue: state.amountDue.toString());
+              return TextFormField(
+                initialValue: state.amountDue.toString(),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  DecimalTextInputFormatter()
+                ],
+              );
               // return Text('${state.amountDue}');
             },
           ),
