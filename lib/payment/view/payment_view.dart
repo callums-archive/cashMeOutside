eac106cc81cabff50fa4e7bdb49ae32ad1f6efb3
Callum Fleming <howzitcallum@gmail.com>
2020-08-21 20:46:07 +0200

    feat(payment interface): payment interface almost complete, needs cleanup and refactor

--- lib/payment/view/payment_view.dart
+++ lib/payment/view/payment_view.dart
@@ -11,19 +11,65 @@ class PaymentView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Payment")),
-        body: Center(
-          child: BlocBuilder<PaymentBloc, PaymentState>(
-            builder: (context, state) {
-              return TextFormField(
-                initialValue: state.amountDue.toString(),
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  DecimalTextInputFormatter()
-                ],
-              );
-              // return Text('${state.amountDue}');
-            },
-          ),
-        ));
+        body: Column(children: <Widget>[
+          Container(
+              padding: EdgeInsets.all(20.0),
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "Amount Due: ",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+                BlocBuilder<PaymentBloc, PaymentState>(
+                    builder: (context, state) {
+                  return Text(
+                    "R${state.amountDue}",
+                    style:
+                        TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                  );
+                })
+              ])),
+          Expanded(
+              child: Center(
+                  child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: BlocBuilder<PaymentBloc, PaymentState>(
+              builder: (context, state) {
+                return TextFormField(
+                  style: TextStyle(fontSize: 25.0),
+                  decoration: InputDecoration(
+                    hintText: "Payment Amount",
+                    isDense: true,
+                    prefixIcon: Text(
+                      "R ",
+                      style: TextStyle(fontSize: 30.0),
+                    ),
+                    prefixIconConstraints:
+                        BoxConstraints(minWidth: 0.0, minHeight: 0),
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    DecimalTextInputFormatter()
+                  ],
+                );
+              },
+            ),
+          ))),
+          Container(
+              child: SizedBox(
+                  width: double.infinity,
+                  height: 60.0,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Process",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    color: Colors.greenAccent,
+                  )))
+        ]));
   }
 }
